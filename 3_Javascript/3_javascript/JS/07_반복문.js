9945ef2422dbe8ab74c13870a5464ab15ee6b9c2
gorru135 <gggorru@gmail.net>
2024-01-24 17:30:04 +0900

    homework

--- 3_Javascript/3_javascript/JS/07_반복문.js
+++ 3_Javascript/3_javascript/JS/07_반복문.js
@@ -92,8 +92,113 @@ function executeFn2(){
     ul.innerHTML = "";
     for(let num=start; num<=end; num+=val){
         ul.innerHTML += `<li>${num}</li>`;
-        
-        
     }
     
+}
+
+/* 다음 모양 출력하기
+12345
+12345
+12345
+*/
+
+function check8(){
+    //4바퀴 반복하는 for문
+    for(let y = 1; y <= 4; y++){
+        // "12345" 출력하는 구문 
+        let str ="";
+        for(let x =1; x<=5; x++){
+            str += x;
+        }
+        console.log(str);
+    }
+}
+
+function check9(){
+
+    for(let y=1; y <= 5; y++){
+
+        let str = "";
+        for(let x= 1; x<=y; x++){
+            str += x;
+        }
+        console.log(str);
+    }
+}
+
+//while 문 확인
+
+function check16(){
+    // 변수를 선언만 해두기
+    let val; // undefined
+
+    // 취소를 누르기 전 까지 반복 
+    // == 취소를 누르면 반복하지 않겠다.
+    while(val !== null) {
+        // 동일 비교 연산자 
+        // != 값이 다른 경우 
+        // !== 값,자료형이 모두 다른경우 true
+        // === 값,자료형이 모두 같은경우 true
+
+        val = prompt("입력 후 확인"); // 변수에 pront값 대입
+        // 확인 -> 입력한 값
+        // 취소 -> null
+        console.log(val);
+    }
+}
+
+//메뉴 주문하기 
+function check17(){
+
+    // 메뉴 가격
+    const gimbap = 3000;
+    const ramen = 3500;
+    const donkkaseu = 5000;
+
+    // 주문 개수 카운트 
+    let gCount = 0; // 김밥 
+    let rCount = 0; // 라면
+    let dCount = 0; // 돈까스
+
+    // pronmpt로 입력할 값을 저장할 변수 선언
+    let input; // undefined
+
+    while(input !== null){// 취소 누르기 전까지 반복
+        input = prompt("메뉴 번호를 입력하세요!");
+
+        switch(input) {
+            case "1" : gCount++; break;
+            case "2" : rCount++; break;
+            case "3" : dCount++; break;
+            case null : alert("주문완료!"); break;
+            default : alert("메뉴에 작성된 번호만 입력해 주세요"); break;
+        }
+    }
+
+    alert(` 김밥 : ${gcount}, 라면 : ${rCount}, 돈까스 : ${dCount}`);
+    let sum = (gCount * gimbap) + (rCount * ramen) + (dCount * donkkaseu)
+
+    alert(`총 가격 : ${sum} 원`)
+}
+
+function check18(){
+    // 1부터 10까지 출력 
+
+    let num = 1;
+    while(num < 11){
+        console.log(num); 
+
+        num++;
+    }
+    console.log("------------------")
+
+    // 10부터 1까지 1씩 감소 (while)
+
+    let x= 10;
+
+    while( x > 0){
+        console.log(x);
+        x--;
+    }
+
 }
--- 3_Javascript/3_javascript/JS/숙제.js
+++ 3_Javascript/3_javascript/JS/숙제.js
@@ -1,13 +1,28 @@
-
 function executeFn3(){
 
-  
+    // 입력받은 숫자
+    let inputNumber = Number(document.getElementById("input3").value);
+
+    // 구구단을 표시할 ul 태그
     const ul = document.getElementById("result3");
 
-    for(let num=1; num<=9; num*2){
-        num *= num; 
-        ul.innerHTML *= `<li>${num}</li>`;
-    }  
-   
+    ul.innerHTML = '';    
 
+    // 반복문
+    // num 은 1부터 시작해서 9까지 1씩 커진다
+    // num++ 은 num = num+1 과 같은 의미 
+    if(input < 2 || input > 9){
+        alert("2~9 사이만 입력해 주세요");
+        return;
+    }
+    
+    for(let num=1; num<=9; num++){
+        
+       
+        
+        // result = 입력받은 숫자 * num 
+        let result = inputNumber * num; 
+        ul.innerHTML += `<li>${inputNumber} x ${num} = ${inputNumber*num} </li>`;
+    } 
 }
+
